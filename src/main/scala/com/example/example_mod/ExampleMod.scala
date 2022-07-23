2fc7d5d50e222f58920caef1b4178ddc26224554
0xmycf <mycf.mycf.mycf@gmail.com>
2022-07-23 18:21:46 +0200

    adapter -> object

--- src/main/scala/com/example/example_mod/ExampleMod.scala
+++ src/main/scala/com/example/example_mod/ExampleMod.scala
@@ -3,7 +3,7 @@ package com.example.example_mod
 import org.quiltmc.loader.api.ModContainer
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer
 
-class ExampleMod extends ModInitializer:
+object ExampleMod extends ModInitializer:
   override def onInitialize(mod: ModContainer): Unit = {
     ExampleModLogger.LOGGER.info("Hello Quilt world from {}!", mod.metadata.name)
   }
--- src/main/scala/com/language_adapter/ScalaAdapter.scala
+++ src/main/scala/com/language_adapter/ScalaAdapter.scala
@@ -1,15 +1,16 @@
 package com.language_adapter
 
+
 import org.quiltmc.loader.api.{LanguageAdapter, ModContainer}
+import scala.language.postfixOps
 
 class ScalaAdapter extends LanguageAdapter {
   override def create[T](mod: ModContainer, value: String, aClass: Class[T]): T = {
     try {
       Class.forName(value + "$").getField("MODULE$").get(null).asInstanceOf[T]
     } catch {
-      case _: Exception =>
-        println(s"Unable to find ${aClass.getName}$$MODULE$$")
-        aClass.getConstructor().newInstance()
+      case e: Exception =>
+        throw Exception("Adapter error...\n" + e)
     }
   }
 }
